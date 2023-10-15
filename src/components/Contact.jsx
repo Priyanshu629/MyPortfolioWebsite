6a35c451b97206d5ee8d7400644d38d8d8247ab6
priyanshu kumar mishra <mishrapriyanshu793@gmail.com>
2023-10-15 07:12:23 +0530

    retifying contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { ToastContainer,toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 
  const Contact = () => {
-  const form = useRef();
- 
+
+  const[name,setName]=useState('')
+  const[email,setEmail]=useState('')
+  const[msg,setMsg]=useState('')
+  
 
   const sendEmail = (e) => {
     e.preventDefault();
-    form.current.reset()
+    
+    let form=document.querySelector('form')
+    setName('')
+    setEmail('')
+    setMsg('')
 
-    if(form.current.value){
-  
-    emailjs.sendForm('service_jao72wc', 'template_m57nk3a', form.current, '13dAVoPothvOlgySx')
+    if(form[0].value==='' || form[1].value==='' || form[2].value===''){
+      return toast.warn("Please fill  all the fields")
+    }
+      emailjs.sendForm('service_jao72wc', 'template_m57nk3a', form, '13dAVoPothvOlgySx')
       .then((result) => {
           console.log(result.text);
           toast.success('Message sent Successfully')
@@ -21,25 +29,23 @@ import 'react-toastify/dist/ReactToastify.css'
       }, (error) => {
           console.log(error.text);
       });
-    }
-    else{
-        toast.warn('All the fields are required',)
-    }
+        
+    
      
   };
 
   return (
     <div className='container' id='contact'>
        
-    <form ref={form} onSubmit={sendEmail} >
+    <form  onSubmit={sendEmail} >
     <h1>Contact Me</h1>
       <label>Name</label>
-      <input type="text" name="from_name" placeholder='Enter your name' />
+      <input type="text" value={name} onChange={(e)=>setName(e.target.value)} name="from_name" placeholder='Enter your name' />
       <label>Email</label>
-      <input type="email" name="from_email" placeholder='Enter your email'/>
+      <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} name="from_email" placeholder='Enter your email'/>
       <label>Message</label>
-      <textarea name="message" placeholder='Enter your message'/>
-      <button>Send</button>
+      <textarea name="message" value={msg} onChange={(e)=>setMsg(e.target.value)} placeholder='Enter your message'/>
+      <button type='submit'>Send</button>
       
     </form>
     <ToastContainer position='top-center' autoClose={3000} theme='light'
