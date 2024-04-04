41a75fa16a4f4c65f984b503c95d32e586649962
priyanshu kumar mishra <mishrapriyanshu793@gmail.com>
2024-04-04 16:59:25 +0530

    updated project section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,38 +1,37 @@
-import React, { useEffect } from "react";
-import img from "./image.jpg";
-
+const img = "https://avatars.githubusercontent.com/u/104246169?v=4";
 
 const About = () => (
   <div className="about">
-
-
     <div className="info">
-      <div id="h1"><h1>Hello Everyone</h1></div>
-      
+      <div id="h1">
+        <h1>Hello Everyone</h1>
+      </div>
+
       <p>
-        My name is Priyanshu kumar mishra and I live in kolkata , West Bengal . I am a computer science student and completed my bachleors drgree in B.sc computer Science from University of Calcutta. I am very much passionate about Website Development and want to see myself at a higher position in IT companies in future
+        My name is Priyanshu kumar mishra and I live in kolkata , West Bengal .
+        I am a computer science student and completed my bachleors degree in
+        B.sc computer Science from University of Calcutta. I am very much
+        passionate about Website Development and want to see myself at a higher
+        position in IT companies in future
       </p>
-      <a href="#contact" id="btn">Contact Me</a>
+      <a href="#contact" id="btn">
+        Contact Me
+      </a>
       <a href="https://github.com/Priyanshu629" target="_blank">
-          
-          <i
-            className="fa-brands fa-square-github xl-2"
-            style={{ color: "white" }}
-          ></i>
-        </a>
-        <a href="https://linkedin.com/in/priyanshu-mishra6291" target="_blank">
-          <i
-            className="fa-brands fa-linkedin xl-2"
-            style={{ color: "white" }}
-          ></i>
-        </a> 
-        
+        <i
+          className="fa-brands fa-square-github xl-2"
+          style={{ color: "white" }}
+        ></i>
+      </a>
+      <a href="https://linkedin.com/in/priyanshu-mishra6291" target="_blank">
+        <i
+          className="fa-brands fa-linkedin xl-2"
+          style={{ color: "white" }}
+        ></i>
+      </a>
     </div>
 
-   
-   <img src={img} alt="" id="img" />
-
-  
+    <img src={img} alt="" id="img" />
   </div>
 );
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,26 +18,67 @@ const Projects = () => {
         >
           <li>
             {" "}
-            This is a simple webapp in which you can find the deatils of any
+            This is a simple webapp in which you can find the details of any
             github users by entering their username.{" "}
           </li>
           <li>
-            I have used html & css for designing and javascript,fetch API and
-            github users API for building this project{" "}
+            I have used React and css for designing and development and github
+            users API for building this project{" "}
           </li>
         </ul>
 
         <br />
         <div className="techstack">
           <h3>Tech Used </h3>
-          <i className="fa-brands fa-html5" style={{color: "#ab1c12"}}></i>
-          <i className="fa-brands fa-css3-alt" style={{color: "#2a6ee5"}}></i>
-          <i className="fa-brands fa-square-js" style={{color: "#bbb61b"}}></i>
+          <i className="fa-brands fa-react" style={{ color: "blue" }}></i>
+          <i className="fa-brands fa-css3-alt" style={{ color: "#2a6ee5" }}></i>
+
           <i className="fa-brands fa-github" style={{ color: "white" }}></i>
           <i className="fa-brands fa-git-alt" style={{ color: "white" }}></i>
           <br />
         </div>
-        <a href="https://priyanshu629.github.io/github-api-project" target="_blank">Show Me</a>
+        <a href="https://github-api-user-finder.netlify.app/" target="_blank">
+          Live
+        </a>
+        <a href="https://github.com/Priyanshu629/Github-User-Finder" target="_blank">
+          Code
+        </a>
+      </div>
+
+      <div className="project1" data-aos="fade-right">
+        <h2>Food Ordering Website</h2>
+
+        <ul
+          style={{
+            fontFamily: "cursive",
+            fontSize: "20px",
+            padding: "5px",
+            fontWeight: "bold",
+            color: "black",
+          }}
+        >
+          <li> A Food ordering website like swiggy</li>
+          <li>A website built using React.</li>
+        </ul>
+
+        <br />
+        <div className="techstack">
+          <h3>Tech Used </h3>
+          <i className="fa-brands fa-react" style={{ color: "blue" }}></i>
+          <i className="fa-brands fa-css3-alt" style={{ color: "#2a6ee5" }}></i>
+          <i className="fa-brands fa-github" style={{ color: "white" }}></i>
+          <i className="fa-brands fa-git-alt" style={{ color: "white" }}></i>
+          <br />
+        </div>
+        <a href="https://my-food-express.netlify.app" target="_blank">
+          Live
+        </a>
+        <a
+          href="https://github.com/Priyanshu629/React-Food-App"
+          target="_blank"
+        >
+          Code
+        </a>
       </div>
 
       <div className="project1" data-aos="fade-right">
@@ -54,23 +95,30 @@ const Projects = () => {
         >
           <li>
             {" "}
-           A heritage website in which you will get information about top 10 heritage places in Kolkata where you can visit.
+            A heritage website in which you will get information about top 10
+            heritage places in Kolkata where you can visit.
           </li>
-          <li>
-            This is simple static website made using HTML and CSS.
-          </li>
+          <li>This is simple static website made using HTML and CSS.</li>
         </ul>
 
         <br />
         <div className="techstack">
           <h3>Tech Used </h3>
-          <i className="fa-brands fa-html5" style={{color: "#ab1c12"}}></i>
-          <i className="fa-brands fa-css3-alt" style={{color: "#2a6ee5"}}></i>
+          <i className="fa-brands fa-html5" style={{ color: "#ab1c12" }}></i>
+          <i className="fa-brands fa-css3-alt" style={{ color: "#2a6ee5" }}></i>
           <i className="fa-brands fa-github" style={{ color: "white" }}></i>
           <i className="fa-brands fa-git-alt" style={{ color: "white" }}></i>
           <br />
         </div>
-        <a href="https://heritageofkolkata.netlify.app" target="_blank">Show Me</a>
+        <a href="https://heritageofkolkata.netlify.app" target="_blank">
+          Live
+        </a>
+        <a
+          href="https://github.com/Priyanshu629/heritageWebsite"
+          target="_blank"
+        >
+          Code
+        </a>
       </div>
     </div>
   );
